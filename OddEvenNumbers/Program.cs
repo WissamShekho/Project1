e06540d40cfda9afe6af2c7464e457b95c0f71ae
Wissam Shekho <Shekhowesam91@gmail.com>
2025-05-27 23:19:00 +0300

    Simplified code

--- OddEvenNumbers/Program.cs
+++ OddEvenNumbers/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 List<int> Numbers = new List<int>() { 4, 1, 6, 3, 2, 9, 5, 7, 8, 0 };
 
+
 Console.WriteLine("Even Numbers: ");
 List<int> evenNumbers = EvenNumbers(Numbers); //gets all even numbers in a list
 
@@ -16,11 +17,9 @@ PrintNumbers(oddNumbers); // printing result: 1, 3, 9, 5, 7
 static void PrintNumbers(List<int> numbers)
 {
     foreach (var n in numbers)
-    {
         Console.Write(n + " ");
-    }
-    Console.WriteLine();
 
+    Console.WriteLine();
 }
 
 static List<int> OddNumbers(List<int> numbers)
@@ -30,18 +29,13 @@ static List<int> OddNumbers(List<int> numbers)
     if (numbers != null)
     {
         foreach (var num in numbers)
-        {
             if (num % 2 == 1)
-            {
                 Odds.Add(num);
-            }
-        }
+       
         return Odds;
     }
     else
-    {
         return null!;
-    }
 }
 
 static List<int> EvenNumbers(List<int> numbers)
@@ -51,17 +45,12 @@ static List<int> EvenNumbers(List<int> numbers)
     if (numbers != null)
     {
         foreach (var num in numbers)
-        {
             if (num % 2 == 0)
-            {
                 Evens.Add(num);
-            }
-        }
+
         return Evens;
     }
     else
-    {
         return null!;
-    }
 }
 
